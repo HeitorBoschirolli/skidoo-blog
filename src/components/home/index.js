8ac801fd7269ae141497c5bb28c75c5fb3757cba
Heitor Boschirolli <heitor.boschirolli@gmail.com>
2020-12-01 22:34:07 -0300

    Small refactor

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,36 +1,16 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import { StyledAppBar } from './styled';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
-
+import { StyledToolBar, StyledTypography } from './styled';
+import { AppBar } from '@material-ui/core';
 
 const Home = () => {
-  const classes = useStyles();
-
   return (
-    <div className={classes.root} color="red">
-      <StyledAppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" className={classes.title}>
-            Skidoo Blog
-          </Typography>
-        </Toolbar>
-      </StyledAppBar>
-    </div>
+    <AppBar position="static">
+      <StyledToolBar>
+        <StyledTypography variant="h6">
+          Skidoo Blog
+        </StyledTypography>
+      </StyledToolBar>
+    </AppBar>
   );
 };
 
--- src/components/home/styled.js
+++ src/components/home/styled.js
@@ -1,7 +1,16 @@
 import styled from "styled-components";
 import AppBar from '@material-ui/core/AppBar';
 import { colors } from "../../helpers/colors";
+import { Toolbar, Typography } from "@material-ui/core";
 
 export const StyledAppBar = styled(AppBar)`
+    background-color: ${colors.black};
+`;
+
+export const StyledTypography = styled(Typography)`
+    flex-grow: 1;
+`;
+
+export const StyledToolBar = styled(Toolbar)`
     background-color: ${colors.black};
 `;
