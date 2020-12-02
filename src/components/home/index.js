76962760c57e5c2048daf68b0843d1de6ccebce8
Heitor Boschirolli <heitor.boschirolli@gmail.com>
2020-12-01 21:02:31 -0300

    Change some colors

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -3,8 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
+import { StyledAppBar } from './styled';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -24,13 +23,13 @@ const Home = () => {
 
   return (
     <div className={classes.root} color="red">
-      <AppBar position="static">
+      <StyledAppBar position="static">
         <Toolbar>
           <Typography variant="h6" className={classes.title}>
             Skidoo Blog
           </Typography>
         </Toolbar>
-      </AppBar>
+      </StyledAppBar>
     </div>
   );
 };
---
+++ src/components/home/styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+import AppBar from '@material-ui/core/AppBar';
+import { colors } from "../../helpers/colors";
+
+export const StyledAppBar = styled(AppBar)`
+    background-color: ${colors.black};
+`;
