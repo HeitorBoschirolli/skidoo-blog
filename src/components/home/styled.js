f253e0566708d24a61209085a485d0a3410b857a
Heitor Boschirolli <heitor.boschirolli@gmail.com>
2020-12-01 22:35:05 -0300

    Remove unused export

--- src/components/home/styled.js
+++ src/components/home/styled.js
@@ -3,10 +3,6 @@ import AppBar from '@material-ui/core/AppBar';
 import { colors } from "../../helpers/colors";
 import { Toolbar, Typography } from "@material-ui/core";
 
-export const StyledAppBar = styled(AppBar)`
-    background-color: ${colors.black};
-`;
-
 export const StyledTypography = styled(Typography)`
     flex-grow: 1;
 `;
